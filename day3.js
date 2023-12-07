e176eca054f979150d78124337f6798b1065879f
ketakikaranjkar <ketaki1karanjkar@gmail.com>
2023-12-07 21:27:39 +0530

    day-3-1

---
+++ day3.js
@@ -0,0 +1,59 @@
+let kK = null;
+console.log(kK);
+
+let oneNo = 5;
+let twoNo = 2;
+let sum = oneNo + twoNo;
+let diff = oneNo - twoNo;
+let mult = oneNo * twoNo;
+let div = oneNo / twoNo;
+let remainder = oneNo % twoNo;
+let powerOf = oneNo ** twoNo;
+
+console.log(sum, diff, mult, div, remainder, powerOf);
+
+console.log(3 > 2);
+console.log(3 >= 2);
+console.log(3 < 2);
+console.log(3 != 2);
+console.log(NaN == NaN);
+console.log(undefined == null);
+console.log(undefined === null);
+
+//length is define from a value charactors// result = false
+let mango = "sweet";
+let avocado = "sourr";
+console.log(mango.length == avocado.length);
+
+//pre -increment
+let count = 7;
+console.log(++count);
+console.log(count);
+
+let counT = 7;
+console.log(++counT);
+console.log(counT);
+
+//     **Increment Oprator**
+//post-increment
+let cOunt = 7;
+console.log(cOunt++);
+console.log(cOunt);
+
+let money = 11;
+console.log(money);
+console.log(money++);
+console.log(++money);
+
+// ** decrement Operator **
+// Pre Decrement
+let moneY = 40;
+console.log(--moneY);
+console.log(moneY);
+
+//post decrement
+let monEY = 40;
+console.log(monEY);
+console.log(monEY--);
+console.log(monEY);
+console.log(--monEY);
