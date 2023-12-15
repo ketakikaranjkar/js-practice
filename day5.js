ea9a4ef7a415529b652cbff7e01aae990a362dcd
ketakikaranjkar <ketaki1karanjkar@gmail.com>
2023-12-15 23:19:22 +0530

    Day-5

--- day5.js
+++ day5.js
@@ -411,3 +411,8 @@ dress.splice(0);
 console.log(dress);
 
 // ** Exercise: Level 2**
+// First remove all the punctuations and change the string to array and count the number of words in the array
+let text =
+  "I love teaching and empowering people. I teach HTML, CSS, JS, React, Python.";
+console.log(text);
+console.log(text.length);
