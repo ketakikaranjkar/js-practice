9847e7cd5905b0ea80e0b90011f6fee454a471fa
ketakikaranjkar <ketaki1karanjkar@gmail.com>
2023-12-27 17:53:53 +0530

    day7-2

--- day7.js
+++ day7.js
@@ -75,3 +75,13 @@ function allNuM(arr) {
     sUm = sUm;
   }
 }
+const changeToUpperCase = arr => {
+  const newArr = [];
+  for (const element of arr) {
+    newArr.push(element.toUpperCase());
+  }
+  return newArr;
+};
+
+const countries = ["Finland", "Sweden", "Norway", "Denmark", "Iceland"];
+console.log(changeToUpperCase(countries));
