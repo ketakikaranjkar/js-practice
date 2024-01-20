4638e6589208ab871d6ee9e5e2d4f84232cd3ae3
ketakikaranjkar <ketaki1karanjkar@gmail.com>
2024-01-20 12:28:21 +0530

    clean

--- web_techs.js
+++
@@ -1,8 +0,0 @@
-const fff = ["HTML", "CSS", "JavaScript", "recat", "redux"];
-const indexx = fff.indexOf("Sass");
-if (indexx !== -1) {
-  console.log("Sass is a preprocess");
-} else {
-  fff.push("Sass");
-  console.log(fff);
-}
